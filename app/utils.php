a8f92a1e9d36312876e20f00c09d35d2e6432d66
emilio <emilio@360-PC-01.360hsllc.local>
2016-11-11 11:23:24 -0500

    add function isPrime($n)

--- app/utils.php
+++ app/utils.php
@@ -1,5 +1,5 @@
 <?php
-ini_set('memory_limit','512M');
+ini_set('memory_limit', '512M');
 $start = microtime(true);
 /*
   Utilities functions to solve the problems
@@ -19,7 +19,35 @@ function isCommandLineInterface()
 
 function print_array($array)
 {
-    echo !isCommandLineInterface() ? '<pre>' : '';
+    echo!isCommandLineInterface() ? '<pre>' : '';
     print_r($array);
-    echo !isCommandLineInterface() ? '</pre>' : '';
+    echo!isCommandLineInterface() ? '</pre>' : '';
+}
+
+function isPrime($n)
+{
+    if ($n == 1) {
+        return false;
+    } else if ($n < 4) {
+        return true; //2 and 3 are prime
+    } else if ($n % 2 == 0) {
+        return false;
+    } else if ($n < 9) {
+        return true; //we have already excluded 4,6 and 8.
+    } else if ($n % 3 == 0) {
+        return false;
+    } else {
+        $r = round(sqrt($n)); // n rounded to the greatest integer r so that r*r<=n
+        $f = 5;
+        while ($f <= $r) {
+            if ($n % $f == 0) {
+                return false;
+            }
+            if (($n % ($f + 2)) == 0) {
+                return false;
+            }
+            $f = $f + 6;
+        }
+        return true;
+    }
 }
